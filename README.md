a693e0c868ca3a08c438e4c6daceddb994f04a24
Samih Omer <samihomer3@hotmail.com>
2019-06-15 19:33:16 +0200

    Update README & Add Heroku Procfile.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# telegram-quickstart
-A quickstart to create a telegram Bot based on Apache Camel.
+# TelegramBot Github Notifier
+Based on Nicola Ferraro's [quickstart repo](https://github.com/nicolaferraro/telegram-quickstart). This project provides a simple telegram bot integration for Github Notifications.
 
-The quickstart can work without any modification to the code if you just put your Telegram authorization token into the [application.properties](/src/main/resources/application.properties) file.
+The premise is simple; you (the user) should create a [telegram bot](https://core.telegram.org/bots). During the bot creation process take note of the HTTP API access token (Lets call it TELEGRAM_AUTH_TOKEN). Then head over to your Github account and generate a [personal access token](https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line) (Lets call this one GITHUB_AUTH_TOKEN). Grant it at least the notifications & repo privileges. Finally, start a chat with your telegram bot and get that chat id (Unfortunately this requires a bit of creativity and Googling from your part). Lets assume you did it and now we call that final parameter TELEGRAM_CHAT_ID.
 
-You can change the [Bot](src/main/java/com/example/Bot.java) class to customize the behavior.
+As for how to run it, refer to the [Heroku's Procfile] (Procfile).
--- src/main/java/com/example/CamelRouter.java
+++ src/main/java/com/example/CamelRouter.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,18 +19,21 @@ public class CamelRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        from("telegram:bots")
+        from("telegram:bots/{{TELEGRAM_AUTH_TOKEN}}")
                 .bean(bot,"echoBack")
-                .to("telegram:bots");
+                .to("telegram:bots/{{TELEGRAM_AUTH_TOKEN}}");
 
         from("timer://fetch_github_notifications?fixedRate=true&period=10s")
-                .setHeader("Authorization", constant("token {{camel.github.authorization-token}}"))
+                .setHeader("Authorization", constant("token {{GITHUB_AUTH_TOKEN}}"))
                 .setHeader(Exchange.HTTP_METHOD, constant("GET"))
                 .to("https4://api.github.com/notifications")
                 .unmarshal().json(JsonLibrary.Jackson, GithubNotification[].class)
                 .bean(bot,"processGithubNotifications")
                 .removeHeaders("*")
-                .setHeader("CamelTelegramChatId",constant("{{camel.telegram.chat-id}}"))
-                .to("telegram:bots");
+                .setHeader("CamelTelegramChatId",constant("{{TELEGRAM_CHAT_ID}}"))
+                .to("telegram:bots/{{TELEGRAM_AUTH_TOKEN}}");
+
+        from("jetty:http://0.0.0.0:{{port}}?matchOnUriPrefix=true")
+            .log(LoggingLevel.INFO,"JETTY MESSAGE");
     }
 }
